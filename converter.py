670a80c63b29632cc2e30b4f52360ca8f7328643
Leo Turowski <leo@turos.de>
2020-03-29 16:53:58 +0200

    Made a Salad using a few pickles and completed funtionality of some funtions

--- converter.py
+++ converter.py
@@ -13,7 +13,6 @@ from zipfile import ZipFile
 import pandas as pd
 import numpy as np
 import pickle
-import matplotlib.pyplot as plt
 
 
 
@@ -26,6 +25,7 @@ def zip_to_csv(path):
     this function converts a single .sql.zip file into a single .csv file
     by first extracting its content and then converting the resulting .sql file
     into a .csv file. This will create both a .sql and a .csv file
+    THIS FUNCTION WORKS AS INTENDED
     :param path: the path of the file, that is to be converted
     """
     os.chdir(os.path.dirname(path))
@@ -41,6 +41,7 @@ def zip_to_npy(path):
     this function converts a single .sql.zip file into a single .numpy file
     by first extracting its content and then converting the resulting .sql file
     into a .numpy file. This will create both a .sql and a .numpy file
+    THIS FUNCTION WORKS AS INTENDED
     :param path: the path of the file, that is to be converted
     """
     os.chdir(os.path.dirname(path))
@@ -56,13 +57,13 @@ def sql_to_csv(path):
     this function takes the path to a .sql file and saves the data of Insert statements 
     in a single .csv file as well as saving all additional information in the file 
     in a single .p pickle list
+    THIS FUNCTION WORKS AS INTENDED
     :param path: the path of the file that is to be converted
     """
     os.chdir(os.path.dirname(path))
     filename = ntpath.basename(path)
     with open(filename, 'r') as oldfile:
-        newfilename = filename[:-3]
-        with open(newfilename + 'csv', 'w') as newfile:
+        with open(filename[:-3] + 'csv', 'w',newline='') as newfile:
             content = oldfile.readlines()
             data = []
             picklelist = []
@@ -71,15 +72,13 @@ def sql_to_csv(path):
                     line = line.split('(')
                     line = line[1]  # cuts of the Insert part of the sql statement
                     line = line[:-3]  # cuts of the ");\n" end of the sql statement
-                    line += "\n" 
-                    line = line.replace("'", "")
                     data.append(line)
                 else:   
                     picklelist.append(line)
 
-                write = csv.writer(newfile, delimiter=',', quoting=csv.QUOTE_ALL)
-                write.writerow(data)
-                pickle.dump(picklelist, open((newfilename + 'p'),'wb'))
+            write = csv.writer(newfile,delimiter='\n',quotechar='#')
+            write.writerow(data)
+            pickle.dump(picklelist, open((filename[:-3] + 'p'),'wb'))
 
 def sql_to_npy(path):
     """
@@ -88,7 +87,7 @@ def sql_to_npy(path):
     as the .sql file it is called upon.
     Additional information is also saved in a single .p pickle list
     :param path: the path of the file that is to be converted
-    """
+    """#Problem die Zeiten werden gelöscht
     os.chdir(os.path.dirname(path))
     filename = ntpath.basename(path)
     with open(filename, 'r') as oldfile:
@@ -102,11 +101,12 @@ def sql_to_npy(path):
                 line = line[1]  # cuts of the Insert part of the sql statement
                 line = line[:-3]  # cuts of the ");\n" end of the sql statement
                 line += "\n"
-                line = line.replace("'", "")
+                line = line.replace("','","|")
+                line = line.replace("'","")
                 data.append(line)
             else:
                 picklelist.append(line)
-        nparray = np.genfromtxt(data, delimeter=',',
+        nparray = np.genfromtxt(data, delimiter="|",
                                 missing_values='')
         np.save(newfilename + 'npy', nparray)
         pickle.dump(picklelist, open(newfilename + "p","wb"))
@@ -118,21 +118,25 @@ def csv_to_sql(path):
     This function creates a new .sql file with insert the data specified in the .csv file
     this function also requies a .p file with the same name as the.csv in the same directory 
     in order to work
+    THIS FUNCTION WORKS AS INTENTED
     :param path: this is the path of the .csv file to be converted
     """
     os.chdir(os.path.dirname(path))
     filename = ntpath.basename(path)
-    with open(path, 'r') as oldfile:
+    with open(path, newline='') as oldfile:
         newfilename = filename[:-3]
         picklelist = pickle.load(open(newfilename + "p","rb"))
-        with open(oldfile, 'w') as newfile:
+        table = picklelist[0]
+        table = table[table.rfind(" ") + 1:-1]
+        reader = csv.reader(oldfile,delimiter='\n',quotechar='#')
+        with open(newfilename+ "sql", "w") as newfile:
             newfile.writelines(picklelist)
-            content = oldfile.readlines()
-            table = picklelist[0]
-            table = table[table.rfind(" ") + 1:-1]
-            for line in content:
-                line.replace(",", "','")
-                newfile.write("INSERT INTO %s VALUES('%s');\n", table, line)
+            for line in reader:
+                newfile.write("INSERT INTO ")
+                newfile.write(table)
+                newfile.write(" VALUES(")
+                newfile.write(''.join(line))
+                newfile.write(");\n")
 
 
 def csv_to_npy(path):
@@ -141,13 +145,13 @@ def csv_to_npy(path):
     This function creates a new .npy file with the data of the .csv file
     this .npy file has the same location and name as the .csv file
     :param path: the path of the file that is to be converted
-    """
+    """#Problem die Zeiten werden gelöscht
     os.chdir(os.path.dirname(path))
     filename = ntpath.basename(path)
     newfilename = filename[:-3] + 'npy'
-    data = np.genfromtxt(path, delimeter=',',
-                         missing_values='')
-    newfilename.save(data)
+    data = np.genfromtxt(path, delimiter=',',
+                         missing_values='|')
+    np.save(newfilename,data)
 
 
 def npy_to_sql(path):  # TODO idee erst zu csv dann zu sql
@@ -171,12 +175,13 @@ def npy_to_csv(path):
     **Convert a single .npy file into a .csv file**
     this function creates a new .csv file from the given .npy file
     and saves it at the same location with the same name as the .npy file
+    THIS FUNCTION WORKS AS INTENDED
     :param path: the path of the .npy file
     """
     os.chdir(os.path.dirname(path))
     np_array = np.load(path, 'r')
     filename = ntpath.basename(path)
-    pd.Dataframe(np_array).to_csv(filename[:-3] + 'csv', index=False)
+    pd.DataFrame(np_array).to_csv(filename[:-3] + 'csv', index=False,header=False,quoting=csv.QUOTE_ALL,quotechar="'")
 
 def gen_GAF(path):
     """
@@ -206,6 +211,8 @@ def switchoption(n,path):
             4: sql_to_npy,
             5: csv_to_sql,
             6: csv_to_npy,
+            7: npy_to_sql,
+            8: npy_to_csv,
         }
         function = switcher.get(n,false_input)
         function(path)
@@ -213,11 +220,13 @@ def switchoption(n,path):
 def main():
         path = input("enter path:\n")
         print("zip_to_csv(1)\n")
-        print("zip_to_npy(2) \n")
-        print("sql_to_csv(3) \n")
-        print("sql_to_npy(4) \n")
-        print("csv_to_sql(5) \n")
-        print("csv_to_npy(6) \n")
+        print("zip_to_npy(2)\n")
+        print("sql_to_csv(3)\n")
+        print("sql_to_npy(4)\n")
+        print("csv_to_sql(5)\n")
+        print("csv_to_npy(6)\n")
+        print("npy_to_sql(7)\n")
+        print("npy_to_csv(8)\n")
         n = int(input("what do you want to do:"))
         switchoption(n,path)
 
